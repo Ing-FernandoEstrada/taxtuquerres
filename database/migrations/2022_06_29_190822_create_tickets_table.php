e5eb64b09b80e5881f7d24f2d475754dc2ba27e9
Ing-FernandoEstrada <edestrada@umariana.edu.co>
2022-06-29 21:40:01 -0500

    Commit Fernando 29/06/2022

---
+++ database/migrations/2022_06_29_190822_create_tickets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('number');
+            $table->string('start_destiny');
+            $table->string('end_destiny');
+            $table->timestamp('start_date_time');
+            $table->timestamp('end_date_time')->nullable();
+            $table->foreignId('vehicle_id')->references('id')->on('vehicles')->cascadeOnUpdate()->restrictOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
